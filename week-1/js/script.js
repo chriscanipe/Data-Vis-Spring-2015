a108a9b77d595ba1cdfaa60bf97894cb029d3e07
Chris Canipe <chris.canipe@wsj.com>
2015-01-28 22:12:52 -0600

    adding week 2 notes

--- week-1/js/script.js
+++ week-1/js/script.js
@@ -2,16 +2,17 @@
 
 var theData;
 
-$(document).ready(function() {
-    getData();
-});
+console.log(theData);
 
+getData();
 
 function getData() {
 
 	//AJAX call to retreive our JSON data
 	$.getJSON("js/baseballcard.json", function(data) {
 		theData = data;
+
+		console.log(theData);
 	});
 }
 
